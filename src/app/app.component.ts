255bda4137ba860c53b1824c661d9f7a325f25a1
chandrakanth <chandrakanth.dc@gmail.com>
2023-09-16 22:24:15 +0530

    host pseudo selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from './models/course.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-style-animations';
+
+  courses: Course[] = [
+    { title: 'Angular Course 1', isActive: false },
+    { title: 'Angular Course 2', isActive: false },
+    { title: 'Angular Course 3', isActive: false }
+  ]
 }
